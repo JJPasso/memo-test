5261716eae7f763b602c31e3a27e2954dfc0d814
jjp <juanjosepasso@hotmail.es>
2021-02-11 23:30:01 -0300

    cm 2

---
+++ src/components/Card/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css'
+function Card (){
+    return(
+        <div className="carta">
+
+        </div>
+    )
+}
+export default Card;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+function Navbar() {
+  return (
+    <>
+      <div className="title">React Memo Test</div>
+      <div>
+        <button className="boton-reiniciar">Reiniciar</button>
+      </div>
+      <div className="title">Intentos:</div>
+    </>
+  );
+}
+export default Navbar;
